e791eb7774060643010ea47ea9cdc5ffb1a002c5
Georgy <beridze.georgy@kiu.edu.ge>
2024-03-16 04:01:31 +0400

    Переписано

---
+++ funcs.py
@@ -0,0 +1,89 @@
+import asyncio
+import os
+import re
+
+import aiohttp
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('(\d+)', key)]
+    return sorted(data, key=alphanum_key)
+
+
+def rename_files(input_folder='./input', extension=".html"):
+    for folder_name in os.listdir(input_folder):
+        folder_path = os.path.join(input_folder, folder_name)
+        if os.path.isdir(folder_path):
+            files = sorted_alphanumeric(os.listdir(folder_path))
+            if not files:
+                print(f"В папке {folder_name} нет файлов")
+                continue
+
+            for i, file in enumerate(reversed(files), start=1):
+                old_path = os.path.join(folder_path, file)
+                if os.path.isfile(old_path):
+                    new_filename = str(i) + extension
+                    new_path = os.path.join(folder_path, new_filename)
+                    os.rename(old_path, new_path)
+    return folder_name
+
+
+async def download_image(session, url, path, cnt, progress_bar):
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            file_path = os.path.join(path, f"{cnt}.jpg")
+            with open(file_path, 'wb') as fd:
+                content = await response.read()
+                fd.write(content)
+                progress_bar.update(1)
+    except aiohttp.ClientError as e:
+        print(f"Ошибка при загрузке изображения {url}: {e}")
+
+
+async def download_images_from_file_async(session, file_path, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    total_images = len(lines)
+
+    with tqdm(total=total_images, desc="Загрузка изображений", unit="image") as progress_bar:
+        tasks = []
+        for cnt, line in enumerate(lines, start=1):
+            url = line.strip()
+            tasks.append(download_image(session, url, output_folder, cnt, total_images, progress_bar))
+
+        await asyncio.gather(*tasks)
+
+
+async def downloader_async(path='./photo', links='links.txt'):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    async with aiohttp.ClientSession() as session:
+        await download_images_from_file_async(session, links, path)
+
+
+def grabber(folder_name):
+    path = f"./input/{folder_name}"
+    count = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
+    _ = 0
+    try:
+        with open('links.txt', 'w') as y:
+            for _ in range(count):
+                _ += 1
+                with open(f'{path}\{_}.html', 'r') as x:
+                    soup = BeautifulSoup(x, 'html.parser')
+                    links = soup.select('a[href^="https://sun"]')
+                    for link in links:
+                        x = link.get('href')
+                        y.write(f'{x}\n')
+        summary = sum(1 for _ in open('links.txt', 'r'))
+        return summary
+    except FileNotFoundError:
+        print('Фото не найдены.')
+        return 0
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+import asyncio
 import os
 
-from dowloader import downloader
-from links_grabber import grabber
-from renamer import rename
+from funcs import rename_files, grabber, downloader_async
 
 
-def main():
-    print(''' __      ___  __                 _     _                   _           _                                         
- \ \    / / |/ /                | |   (_)                 | |         | |                                        
-  \ \  / /| ' /    __ _ _ __ ___| |__  ___   _____   _ __ | |__   ___ | |_ ___    _ __   __ _ _ __ ___  ___ _ __ 
-   \ \/ / |  <    / _` | '__/ __| '_ \| \ \ / / _ \ | '_ \| '_ \ / _ \| __/ _ \  | '_ \ / _` | '__/ __|/ _ \ '__|
-    \  /  | . \  | (_| | | | (__| | | | |\ V /  __/ | |_) | | | | (_) | || (_) | | |_) | (_| | |  \__ \  __/ |   
-     \/   |_|\_\  \__,_|_|  \___|_| |_|_| \_/ \___| | .__/|_| |_|\___/ \__\___/  | .__/ \__,_|_|  |___/\___|_|   
-                                                    | |                          | |                             
-                                                    |_|                          |_|                             ''')
+async def main():
+    print('''
+ __      _ _  __                 _     _                   _           _                                        
+ \ \    / / |/ /                | |   (_)                 | |         | |                                       
+  \ \  / /| ' /    __ _ _ __ ___| |__  ___   _____   _ __ | |__   ___ | |_ ___    _ __   __ _ _ __ ___ ___ _ __ 
+   \ \/ / |  <    / _` | '__/ __| '_ \| \ \ / / _ \ | '_ \| '_ \ / _ \| __/ _ \  | '_ \ / _` | '__/ __/ _ \ '__|
+    \  /  | . \  | (_| | | | (__| | | | |\ V /  __/ | |_) | | | | (_) | || (_) | | |_) | (_| | | | (_|  __/ |   
+     \/   |_|\_\  \__,_|_|  \___|_| |_|_| \_/ \___| | .__/|_| |_|\___/ \__\___/  | .__/ \__,_|_|  \___\___|_|   
+                                                    | |                          | |                            
+                                                    |_|                          |_|                            
+                                                                                                        By @E1r0y
+    ''')
+
     input('Press Enter')
-    rename()
+    folder_name = rename_files()
     print('Файлы отсортированы.')
-    amount = grabber()
+    amount = grabber(folder_name)
     print(f'Найдено {amount} фото.')
-    downloader()
-    print(f'Фотографии находятся в /photo')
+    await downloader_async()
+    print('Фотографии находятся в /photo')
     input('Press Enter to close')
 
 
@@ -33,4 +36,4 @@ def clear():
 
 if __name__ == '__main__':
     clear()
-    main()
+    asyncio.run(main())
